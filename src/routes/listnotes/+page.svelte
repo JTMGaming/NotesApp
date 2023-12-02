16807a2e32a36d0c9f01750ab735ee623cbe9d04
JTMGaming <porkkanalaatikko2@gmail.com>
2023-12-02 20:39:42 +0200

    Update +page.svelte

--- src/routes/listnotes/+page.svelte
+++ src/routes/listnotes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+    // @ts-ignore
     let notes = [];
+    let results = [];
+    let currentid = "all";
 
     async function getNotes() {
 		const res = await fetch("https://luentomuistiinpano-api.netlify.app/.netlify/functions/notes");
@@ -9,6 +12,18 @@
 
 		return data;
 	}
+    // @ts-ignore
+
+    function getFilter(){
+        results = notes.filter(
+            (thing, index, self) =>
+                index ===
+                self.findIndex((t) => t.course.id === thing.course.id)
+        );
+    }
+
+    
+    
 </script>
 
 
@@ -16,28 +31,47 @@
 
 <div id="container">
     <p id="text">Course:</p>
-    <button class="courses-button"></button>
+    <section>
+        <select name="menu" id="menu" bind:value={currentid}>
+            <option disabled selected value="">Select a course</option>
+            <option value="all">All courses</option>
+
+            {#await getNotes()}
+            	<p>Loading data...</p>
+            {:then notes}
+                {getFilter()}
+                {#each results as result}
+                    <option value={result.course.id}>{result.course.name}</option>>
+                    {console.log(currentid)}
+                {/each}
+            {:catch error}
+            	<p>{error.message}</p>
+            {/await}
+        </select>
+    </section>
 </div>
 
 
 {#await getNotes()}
 	<p>Loading data...</p>
 {:then notes}
-{#each notes as note (note.id)}
-  <div class="allNotes">
-    <h2>{note.course.name}</h2>
-    <p>{note.text}</p>
-  </div>
-{/each}
+    {#each (currentid === 'all' ? notes : notes.filter(note => note.course.id === currentid)) as note (note.id)}
+        <div class="allNotes">
+            <h2>{note.course.name}</h2>
+            <p>{note.text}</p>
+        </div>
+    {/each}
 {:catch error}
 	<p>{error.message}</p>
 {/await}
+
 
 <style>
     #container{
         display: grid;
         grid-template-columns: auto auto;
         align-items: center;
+        margin-bottom: 5vh;
     }
 
     .courses-button{
@@ -58,6 +92,12 @@
 
     .allNotes{
         display: grid;
-        place-items: center;
+        border-radius: 10px;
+        border-style: solid;
+        margin-bottom: 10px;
+    }
+
+    .allNotes > *{
+        margin-left: 1vw;
     }
 </style>
